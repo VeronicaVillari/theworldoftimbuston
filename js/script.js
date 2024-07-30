105a62ccc8a4b14dad6086a1e50256d7e5b01307
VeronicaVillari <veronicavillari@abacatania.it>
2024-07-30 14:42:03 +0200

    burton

--- js/script.js
+++ js/script.js
@@ -219,4 +219,13 @@ const $$ = str => document.querySelectorAll(str);
     app.carousel.init();
 })();
 
+/*prova burger*/
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navmenu.classList.toggle("active");
+}
